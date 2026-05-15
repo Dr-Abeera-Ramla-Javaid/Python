18adc679bc8a1208c35b2aa86de6ebf845f3cd9a
Abeera Ramla Javaid <dr.arjavaid701.pk@gmial.com>
2026-05-15 11:03:06 +0500

    Commit

--- Guess_Games.py
+++ Guess_Games.py
@@ -1,12 +1,12 @@
-secret_word = "Abeera"
+secret_word = "giraffe"
 guess = ""
 guess_count = 0
 guess_limit = 3
 out_of_guesses = False
 
-while guess != secret_word:
+while guess != secret_word and not(out_of_guesses):
 
-    if guess_count < guess_limit and not out_of_guesses:
+    if guess_count < guess_limit:
         guess = input("Guess a word:")
         guess_count += 1
     else:
